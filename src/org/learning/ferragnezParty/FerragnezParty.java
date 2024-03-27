4005231fdc64c07ae5f85337d369e4881664498f
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-03-27 15:39:14 +0100

    updated FerragnezParty

--- src/org/learning/ferragnezParty/FerragnezParty.java
+++ src/org/learning/ferragnezParty/FerragnezParty.java
@@ -1,7 +1,44 @@
 package org.learning.ferragnezParty;
 
+//imports
+import java.util.Scanner;
+
 public class FerragnezParty {
     public static void main(String[] args) {
-
+        //inizializzo scanner
+        Scanner scanner = new Scanner(System.in);
+        //dichiaro le variabili
+        String[] guests;
+        String fullName;
+        boolean nameInList = false;
+        //inizializzo l'array degli invitati
+        guests = new String[]{"Dua Lipa",
+                "Paris Hilton",
+                "Manuel Agnelli",
+                "J-Ax",
+                "Francesco Totti",
+                "Ilary Blasi",
+                "Bebe Vio",
+                "Luis",
+                "Pardis Zarei",
+                "Martina Maccherone",
+                "Rachel Zeilic"
+        };
+        //chiedo all'utente i nome completo e lo salvo nella variabile
+        System.out.print("your complete name: ");
+        fullName = scanner.nextLine();
+        //se presente nella lista entra
+        int i = 0;
+        while (i < guests.length && !nameInList){
+            if(fullName.equals(guests[i])){
+                //il nome è presente nella lista
+                nameInList = true;
+            }
+            i++;
+        }
+        if(nameInList) System.out.println("puoi entrare");
+        else System.out.println("non puoi entrare");
+        //chiudo scanner
+        scanner.close();
     }
 }
