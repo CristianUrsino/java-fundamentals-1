e96bd2e0648ed82ff15a683f2a7e0ff053f586e3
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-03-27 14:46:40 +0100

    updated TrainTicket

--- src/org/learning/trainTicket/CalcolaBiglietto.java
+++ src/org/learning/trainTicket/CalcolaBiglietto.java
@@ -1,7 +1,32 @@
 package org.learning.trainTicket;
 
+//imports
+import java.util.Scanner;
+
 public class CalcolaBiglietto {
     public static void main(String[] args) {
+        //definisco lo scanner
+        Scanner scanner = new Scanner(System.in);
+        //definisco le variabili
+        int kilometresTravel, age;
+        //inizializzo le variabili tramite lo scanner
+        System.out.print("how many kilometers do you have to travel: ");
+        kilometresTravel = scanner.nextInt();
+        System.out.print("how old are you: ");
+        age = scanner.nextInt();
+        //calcolo il prezzo di default
+        double price = (double) kilometresTravel * 0.21;
+        //in base all'età applico lo sconto
+        if(age >= 65) {
+            //over 65 40% di sconto
+            price *= 0.60;
+        }else if (age <= 18){
+            //under 18
+            price *= 0.80;
+        }
+        System.out.println("final price: " + price + "€");
 
+        //chiudo lo scanner
+        scanner.close();
     }
 }
