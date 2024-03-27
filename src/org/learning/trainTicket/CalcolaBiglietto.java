e05727655ebcdb53822febab7d8506d16f0f0773
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-03-27 16:43:36 +0100

    updated TrainTicket with validation

--- src/org/learning/trainTicket/CalcolaBiglietto.java
+++ src/org/learning/trainTicket/CalcolaBiglietto.java
@@ -8,12 +8,34 @@ public class CalcolaBiglietto {
         //definisco lo scanner
         Scanner scanner = new Scanner(System.in);
         //definisco le variabili
-        int kilometresTravel, age;
-        //inizializzo le variabili tramite lo scanner
-        System.out.print("how many kilometers do you have to travel: ");
-        kilometresTravel = scanner.nextInt();
-        System.out.print("how old are you: ");
-        age = scanner.nextInt();
+        int kilometresTravel = 0, age = 0;
+        //inizializzo e verifico kilometresTravel
+        do {
+            boolean messageOfErrorKilometresTravel = false;
+            try {
+                System.out.print("how many kilometers do you have to travel: ");
+                kilometresTravel = scanner.nextInt();
+            } catch (Exception e) {
+                System.out.println("Invalid input. Please enter a valid number.");
+                messageOfErrorKilometresTravel = true;
+                scanner.next();
+            }
+            if(kilometresTravel <=0 && ! messageOfErrorKilometresTravel) System.out.println("invalid input. Please enter a positive number");
+        }while (kilometresTravel <= 0 );
+        //inzializzo e verifico age
+        do {
+            boolean messageOfErrorAge = false;
+            try {
+                System.out.print("how old are you: ");
+                age = scanner.nextInt();
+            } catch (Exception e) {
+                System.out.println("Invalid input. Please enter a valid integer.");
+                messageOfErrorAge = true;
+                scanner.next();
+            }
+            if(age <=0 && !messageOfErrorAge) System.out.println("invalid input. Please enter a positive number");
+        }while (age <= 0 );
+
         //calcolo il prezzo di default
         double price = (double) kilometresTravel * 0.21;
         //in base all'età applico lo sconto
