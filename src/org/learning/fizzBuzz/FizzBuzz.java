7feeb65865782fa83dbc571f64f83c58efcb4d6f
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-03-28 10:09:51 +0100

    fixed and improved the code  FizzBuzz

--- src/org/learning/fizzBuzz/FizzBuzz.java
+++ src/org/learning/fizzBuzz/FizzBuzz.java
@@ -2,19 +2,19 @@ package org.learning.fizzBuzz;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-        //inizializzo variabili utili
-        int total = 100;
-        int fizz = 3;
-        int buzz = 5;
+        //inizializzo costanti
+        final int TOTAL = 100;
+        final int FIZZ = 3;
+        final int BUZZ = 5;
         //itero tutti i numeri da 0 a total
-        for (int i = 1; i <= total; i++) {
-            if(i % fizz == 0 && i % buzz == fizz){
+        for (int i = 1; i <= TOTAL; i++) {
+            if(i % FIZZ == 0 && i % BUZZ == 0){
                 //multiplo sia di fizz che di buzz
                 System.out.print("FizzBuzz, ");
-            }else if(i % buzz == 0){
+            }else if(i % BUZZ == 0){
                 //multiplo di buzz
                 System.out.print("Buzz, ");
-            }else if(i % fizz == 0){
+            }else if(i % FIZZ == 0){
                 //multiplo di fizz
                 System.out.print("Fizz, ");
             }else{
